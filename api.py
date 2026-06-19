cb116434cfd276e59b10752d0917a026f809cda4
GiovanniTorino <oldgamespciso9090@gmail.com>
2026-06-19 07:47:30 -0300

    api.py

--- api.py
+++ api.py
@@ -31,9 +31,9 @@ FILMES_RAW = [
     ("O Pestinha",             1, 105, 5.5, 2016, 35),
 
     ("Marley e Eu",             2, 140, 8.5, 2021, 75),
-    ("Resgate do soldado Ryan", 2, 125, 7.2, 2019, 55),
+    ("Resgate do Soldado Ryan", 2, 125, 7.2, 2019, 55),
     ("A Culpa É das Estrelas",  2, 135, 9.0, 2023, 90),
-    ("O ultimo Homem",          2, 115, 6.0, 2015, 30),
+    ("O Ultimo Homem",          2, 115, 6.0, 2015, 30),
 
     ("Até a morte",            3,  90, 6.8, 2020, 65),
     ("A Cabana",               3, 100, 7.5, 2022, 80),
